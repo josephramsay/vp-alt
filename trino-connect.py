f74915a9f3115c992f1f37fc41dfc9ec2f4b07f5
Joseph Ramsay <ramsayjose@live.com>
2021-09-28 13:27:16 +1300

    fix: fs.s3 id added

--- trino-connect.py
+++ trino-connect.py
@@ -1,4 +1,5 @@
 import trino
+import json
 
 process = True
 
@@ -8,8 +9,8 @@ tmp_dest = tmp + "/" + projectName
 
 
 trino_conn = trino.dbapi.connect(
-    host='trino2-coordinator-headless',
-    # host='localhost',
+    #host='trino2-coordinator-headless',
+    host='localhost',
     port=8080,
     user='joer',
     catalog='hive',
@@ -17,10 +18,27 @@ trino_conn = trino.dbapi.connect(
 )
 cursor = trino_conn.cursor()
 
-print("Getting " + projectName + " table")
 if process:
-    queryCapture = """
-    SELECT file_name, url, bucket, prefix_processed, prefix_tif, prefix_hs, prefix_slope, prefix_aspect FROM %s
-    """ % (projectName)
-    rows = cursor.execute(queryCapture)
-    print(cursor.fetchall())
+    exclude = ['jdbc','metadata','runtime','sf1','sf100', 'sf1000', \
+        'sf10000', 'sf100000', 'sf300', 'sf3000', 'sf30000','tiny']
+    q1 = 'SELECT file_name, url, bucket FROM {}'.format(projectName)
+    #cursor.execute(q1)
+    #fub = cursor.fetchall()
+    #print (fub)
+
+    qc = 'SHOW CATALOGS'
+    cursor.execute(qc)
+    cats = [c[0] for c in cursor.fetchall()]
+    print (cats)
+    for c in cats:
+        qs = 'SHOW SCHEMAS FROM "{cat}"'.format(cat=c)
+        print (qs)
+        cursor.execute(qs)
+        schs = [s[0] for s in cursor.fetchall() if s[0] not in exclude]
+        print (schs)
+        for s in schs:
+            qt = 'SHOW TABLES FROM "{sch}"'.format(sch=s)
+            print (qt)
+            cursor.execute(qt)
+            tabs = cursor.fetchall()
+            print (tabs)
